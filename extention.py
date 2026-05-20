db2456067f2fd2877304d663c877662e892d4b1f
abrar nadaf <abrarsnadaf@gmail.com>
2026-05-21 00:24:50 +0530

    updated extention.py

--- extention.py
+++ extention.py
@@ -8,3 +8,16 @@ usage: git remote set-url [--push] <name> <newurl> [<oldurl>]
     --[no-]add            add URL
     --[no-]delete         delete URLs
 hello am abrar
+[master 3599e72] updated extension.py
+ 1 file changed, 2 insertions(+), 1 deletion(-)
+PS C:\Users\Abrar Nadaf\Desktop\github> git push
+Enumerating objects: 5, done.
+Counting objects: 100% (5/5), done.
+Delta compression using up to 8 threads
+Compressing objects: 100% (3/3), done.
+Writing objects: 100% (3/3), 326 bytes | 163.00 KiB/s, done.
+Total 3 (delta 1), reused 0 (delta 0), pack-reused 0 (from 0)
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/Abrar060795/github.git
+   5764b94..3599e72  master -> master
+PS C:\Users\Abrar Nadaf\Desktop\github> 
