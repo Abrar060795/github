68f2889712a38f87e77729232dd6d68f7dccc79a
abrar nadaf <abrarsnadaf@gmail.com>
2026-05-20 23:59:12 +0530

    added extention.py

---
+++ extention.py
@@ -0,0 +1,9 @@
+PS C:\Users\Abrar Nadaf\Desktop\github> git remote add origin https://github.com/Abrar060795/github.git
+PS C:\Users\Abrar Nadaf\Desktop\github> git remote -v
+PS C:\Users\Abrar Nadaf\Desktop\github> git remote set-url origin
+usage: git remote set-url [--push] <name> <newurl> [<oldurl>]
+   or: git remote set-url --delete <name> <url>
+
+    --[no-]push           manipulate push URLs
+    --[no-]add            add URL
+    --[no-]delete         delete URLs
